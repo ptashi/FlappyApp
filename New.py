e4ae1fbc18a019ab9b3e53fe7e74dd6cf091d025
RabjamX2 <rabjam123@gmail.com>
2024-04-25 10:52:11 -0400

    clean

--- New.py
+++ New.py
@@ -15,11 +15,13 @@ canvas.pack()
 
 circleVelocity = 0
 
+
 def keyPressed(event):
     if event.char == " ":
         global circleVelocity
         circleVelocity = -JUMP_POWER
- 
+
+
 root.bind("<Key>", keyPressed)
 
 # Create the circle
@@ -31,52 +33,67 @@ gameIsRunning = True
 def moveCircle():
     global gameIsRunning
     global circleVelocity
-    
+
     if not gameIsRunning:
         return
     coordsOfCircle = canvas.coords(circle)
 
     # Gravity
-    circleVelocity += GRAVITY 
+    circleVelocity += GRAVITY
 
-    canvas.move(circle, 0, circleVelocity) # Move the circle right by 0 and down by 1 pixel : Simulating gravity
+    canvas.move(
+        circle, 0, circleVelocity
+    )  # Move the circle right by 0 and down by 1 pixel : Simulating gravity
 
-    
     if coordsOfCircle[3] > HEIGHT or coordsOfCircle[1] < 0:
         gameIsRunning = False
         canvas.create_text(400, 400, text="GAME OVER", fill="red", font=("Impact", 40))
         return
-    
+
     c = canvas.coordsOfCircle(circle)
-    
+
     circle_center_x = (coordsOfCircle[0] + coordsOfCircle[2]) / 2
     circle_center_y = (coordsOfCircle[1] + coordsOfCircle[3]) / 2
     # coordsOfCircle[0] is left, coordsOfCircle[1] is top, coordsOfCircle[2] is right, coordsOfCircle[3] is bottom
     # coords[0] is left, coords[1] is top, coords[2] is right, coords[3] is bottom
-    
-    for pipe in listOfPipes: # pipe = [top_pipe, bottom_pipe]
-        top_pipe_coords = canvas.coords(pipe[0]) 
+
+    for pipe in listOfPipes:  # pipe = [top_pipe, bottom_pipe]
+        top_pipe_coords = canvas.coords(pipe[0])
         bottom_pipe_coords = canvas.coords(pipe[1])
-    
-        if (top_pipe_coords[0] < circle_center_x < top_pipe_coords[2] or bottom_pipe_coords[0] < circle_center_x < bottom_pipe_coords[2]) \
-            and not (top_pipe_coords[1] < circle_center_y < bottom_pipe_coords[3]):
-                print("ITS TOUCHING THE PIPE")
-                print("Circle: ", coordsOfCircle)
-                print("Top Pipe: ", top_pipe_coords)
-                print("Bottom Pipe:", bottom_pipe_coords)
-                canvas.create_text(400, 400, text="GAME OVER", fill="red", font=("Impact", 40))
-                gameIsRunning = False
-                return
+
+        if (
+            top_pipe_coords[0] < circle_center_x < top_pipe_coords[2]
+            or bottom_pipe_coords[0] < circle_center_x < bottom_pipe_coords[2]
+        ) and not (top_pipe_coords[1] < circle_center_y < bottom_pipe_coords[3]):
+            print("ITS TOUCHING THE PIPE")
+            print("Circle: ", coordsOfCircle)
+            print("Top Pipe: ", top_pipe_coords)
+            print("Bottom Pipe:", bottom_pipe_coords)
+            canvas.create_text(
+                400, 400, text="GAME OVER", fill="red", font=("Impact", 40)
+            )
+            gameIsRunning = False
+            return
     root.after(20, moveCircle)
 
 
-
 # Create the pipes
-listOfPipes = [] # List of all the pipes , data formar = [ [top_pipe, bottom_pipe], [top_pipe, bottom_pipe], ... ]
+listOfPipes = (
+    []
+)  # List of all the pipes , data formar = [ [top_pipe, bottom_pipe], [top_pipe, bottom_pipe], ... ]
+
 
 def spawnNewPipe(xOfPipe, widthOfPipe, gapPosition, gapOfPipe, bottomOfTheTopPipe):
-    top_pipe = canvas.create_rectangle(xOfPipe, 0, xOfPipe + widthOfPipe, gapPosition, fill="darkgreen")
-    bottom_pipe = canvas.create_rectangle(xOfPipe, gapPosition + gapOfPipe, xOfPipe + widthOfPipe, HEIGHT, fill="darkgreen")
+    top_pipe = canvas.create_rectangle(
+        xOfPipe, 0, xOfPipe + widthOfPipe, gapPosition, fill="darkgreen"
+    )
+    bottom_pipe = canvas.create_rectangle(
+        xOfPipe,
+        gapPosition + gapOfPipe,
+        xOfPipe + widthOfPipe,
+        HEIGHT,
+        fill="darkgreen",
+    )
     listOfPipes.append([top_pipe, bottom_pipe])
 
 
@@ -87,15 +104,16 @@ gapOfPipe = 100
 bottomOfTheTopPipe = 500
 gapBetweenPipes = 300
 
+
 def movePipes():
     global gapPosition
     if not gameIsRunning:
         return
-    
+
     if len(listOfPipes) == 0:
         spawnNewPipe(xOfPipe, widthOfPipe, gapPosition, gapOfPipe, bottomOfTheTopPipe)
     elif canvas.coords(listOfPipes[-1][0])[0] < WIDTH - gapBetweenPipes:
-        gapPosition = random.randint(100,600)
+        gapPosition = random.randint(100, 600)
         spawnNewPipe(xOfPipe, widthOfPipe, gapPosition, gapOfPipe, bottomOfTheTopPipe)
 
     for pipe in listOfPipes:
@@ -106,12 +124,10 @@ def movePipes():
                 break
             else:
                 canvas.move(pipePart, -1, 0)
-    
-    root.after(10, movePipes)
 
+    root.after(10, movePipes)
 
 
 moveCircle()
 movePipes()
 root.mainloop()
-
